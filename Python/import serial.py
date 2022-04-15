de75cfc422bfc7d5222d68f8d45a37ee1e26e6d7
Abhinav Aggarwal <91041178+abhinavagg17@users.noreply.github.com>
2022-04-14 23:01:09 -0400

    removed unused code and commented all functions

--- Python/import serial.py
+++ Python/import serial.py
@@ -3,26 +3,14 @@ import time
 
 # NOTE: 
 # port name may need to change from COM5 to other COM# depending on the computer usb connection
-# baud rate depends on the rate being used in the arduino sketch (300 is the lowest possible rate)
+# baud rate depends on the rate being used in the arduino sketch
 # the arduino sketch we are working with is "MPU6050_DMP6" example in the MPU6050 subfolder of I2C Arduino documentation
 
-# This code has been moved to the initialized_arduino function
-# baudrate = 300
-
-# arduino1 = serial.Serial(port='COM5', baudrate=baudrate)
-# arduino2 = serial.Serial(port='COM9', baudrate=baudrate)
-
-# we aren't writing to the microcontroller so the write portion of this function does  not matter
-# i think there are methods we can apply after readline() to further process the data as it gets read into python (things like strip, for example)
-# def write_read(arduino, x):
-#     arduino.write(bytes(x, 'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return data
-
+# Opens specified port with specified baudrate. Returns serial.Serial object.
 def initialize_arduino(port, baudrate):
     return serial.Serial(port=port, baudrate=baudrate)
 
+# Calculates the initial offsets for a neutral wrist position based on the first 10 seconds of readings. Returns list of offsets.
 def zero_readings(baudrate, arduino1, arduino2):
     initial_YPR_reads = [[], [], []]
     
@@ -37,15 +25,18 @@ def zero_readings(baudrate, arduino1, arduino2):
 
     return offsets_YPR
 
+# Reads one line of data from the port. Returns the line of data that was read.
 def read(arduino):
    data = arduino.readline()
    return data
 
+# Parses a string in the format "b’yaw pitch roll ‘\t0.90\t-0.04\t0.04\r\n’" to [0.90, -0.04, 0.04]. Returns a list of yaw, pitch, roll angles.
 def parse_read(read):
     read = read.split()
     yaw_pitch_roll_strlist = read[3:6]
     return [float(i) for i in yaw_pitch_roll_strlist]
 
+# Calculates the relative yaw, pitch, roll angles between two lists of yaw, pitch, roll angles. Returns a list of relative yaw, pitch, roll angles.
 def relative_angles(ypr1, ypr2):
     delta_ypr = []
     for i in range(3):
@@ -53,6 +44,7 @@ def relative_angles(ypr1, ypr2):
         delta_ypr.append(delta)
     return delta_ypr
 
+# Reads a line 
 def record_angles(YPR_list, arduino1, arduino2):
     read1 = read(arduino1)
     read2 = read(arduino2)
@@ -64,13 +56,8 @@ def record_angles(YPR_list, arduino1, arduino2):
     YPR_list[1].append(orientation[1])
     YPR_list[2].append(orientation[2])
 
+# Key function of the program. Initializes serial ports. Calculates offsets, sets constants for risk thresholds and risk period. Contains infinite loop that continuously reads from two serial ports, calculates relative angles, determines if angle thresholds have been exceeded, prints warning to console if appropriate. 
 def read_print_loop():
-
-    #print("Enter file name to create and write to:")
-    #file_name = input()
-    #file_name = 'test.txt'
-    #open_file = open(file_name, "a")
-
     baudrate = 10
     arduino1 = initialize_arduino('COM5', baudrate)
     arduino2 = initialize_arduino('COM9', baudrate)
@@ -78,22 +65,23 @@ def read_print_loop():
     offset_YPR = zero_readings(baudrate, arduino1, arduino2)
 
     read_buffer = [[], [], []]
+    # 5 second risk period for testing purposes
     risk_period = 5
+    # Angle thresholds for increased carpal tunnel pressure based on literature reviews
     ulnar_limit = 14.5
     radial_limit = -21.8
     flexion_limit = -48.6
     extension_limit = 32.7
 
-
+    # Infinite loop
     while True:
-        # reading_storage_Y = []
-        # reading_storage_P = []
-        # reading_storage_R = []
         reading_storage_YPR = [[], [], []]
 
+        # Collects baudrate number of readings
         while (len(reading_storage_YPR[0]) < baudrate):
             record_angles(reading_storage_YPR, arduino1, arduino2)
 
+        # Averages baudrate number of readings to get an average reading per second
         Y_per_second = sum(reading_storage_YPR[0]) / baudrate
         P_per_second = sum(reading_storage_YPR[1]) / baudrate
         R_per_second = sum(reading_storage_YPR[2]) / baudrate
@@ -106,9 +94,10 @@ def read_print_loop():
         # print("yaw: " + str(Y_per_second) , "pitch: " + str(P_per_second), "roll: " + str(R_per_second))
         formatted_string = f"x-axis: {R_adjusted}, y-axis: {P_adjusted}, z-axis: {Y_adjusted}"
 
-        print(formatted_string)
-        #open_file.write('test')
+        # Optional line that prints the average wrist angle measurement per second to the console
+        # print(formatted_string)
 
+        # Maintains a list with a length equivalent to the risk_period of the most recent average per second readings
         if (len(read_buffer[0]) < risk_period):
             read_buffer[0].append(Y_adjusted)
             read_buffer[1].append(P_adjusted)
@@ -122,6 +111,7 @@ def read_print_loop():
             read_buffer[1].append(P_adjusted)
             read_buffer[2].append(R_adjusted)
 
+            # Conditional blocks that check if a specific wrist angle exceeded its threshold on average over the risk_period and if the wrist angle is currently exceeding the threshold. If both conditions are true, then an appropriate warning is printed to the console.
             if ((sum(read_buffer[0]) / risk_period) > ulnar_limit and read_buffer[0][-1] > ulnar_limit):
                 print("Ulnar deviation limit exceeded!")
 
